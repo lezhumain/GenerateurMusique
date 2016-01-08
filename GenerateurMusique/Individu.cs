17d8ba88dc172201cfb4289305b2330b9759f164
lezhumain <julien.merion@epsi.fr>
2016-01-08 14:17:57 +0100

    bonchoir

---
+++ GenerateurMusique/Individu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateurMusique
+{
+    public class Individu : MIDISong
+    {
+        //public void Save(MemoryStream objWriter)
+        //{
+        //    base.Save(objWriter);
+        //}
+
+        /// <summary>
+        /// Creates an 'Individu' from a 'MIDISong'
+        /// </summary>
+        /// <param name="song">The MIDISong we create from</param>
+        /// <param name="notes">The array containing the actual songNotes</param>
+        public Individu(MIDISong song) : base(song)
+        {
+            int[] notes = song.Notes;
+
+            int minNotes = notes.Length;
+
+            for (int i = 0; i < minNotes; i++)
+                _notes[i] = notes[i];
+
+        }
+
+        public Individu(Individu papa, Individu maman)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public void Mutate()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public void Play()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GenerateurMusique/Population.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateurMusique
+{
+    public class Population
+    {
+        public static readonly short MAXINDIVIDUS = 10;
+        public static readonly short MUTARATE = 10;
+        public static readonly short CROSSOVER = 5;
+
+        private Individu[] _individus = new Individu[MAXINDIVIDUS];
+
+        public int NbGenerations => 0; // TODO
+
+        public Population()
+        {
+            //TODO
+            for (int i = 0; i < MAXINDIVIDUS; i++)
+            {
+                //_individus[i] = new Individu();
+            }
+        }
+
+        public void SelectParents()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public void Survival()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
